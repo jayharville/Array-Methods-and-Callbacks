51892d5fe5f8f77a451c7636a1ecec4202a0b29a
MacBook <Mac@MacBooks-MacBook-Pro.local>
2020-05-08 10:55:08 -0700

    Completed MVP

--- index.js
+++ index.js
@@ -103,6 +103,7 @@ function getCountryWins(data, teamInitials) {
 
 };
 
+
 const result = getCountryWins(fifaData,"COl");
 
 /* Task 8: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
